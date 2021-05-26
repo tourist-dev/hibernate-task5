af1af6a51f62e365e7ace4ee7c0e76f0c2f89621
tourist-dev <shaik.sameer@orakris.com>
2021-05-26 18:52:54 +0530

    Completed Passenger.java and Train.java

--- src/main/java/org/orakris/Passenger.java
+++ src/main/java/org/orakris/Passenger.java
@@ -1,4 +1,50 @@
 package org.orakris;
 
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
 public class Passenger {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String name;
+    private int age;
+
+    @ManyToMany(targetEntity = Train.class, cascade = {CascadeType.ALL})
+    @JoinTable(name = "Reservation", joinColumns = { @JoinColumn(name = "pid") },
+            inverseJoinColumns = { @JoinColumn(name = "tid") })
+    private List<Train> trainList;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public List<Train> getTrainList() {
+        return trainList;
+    }
+
+    public void setTrainList(List<Train> trainList) {
+        this.trainList = trainList;
+    }
 }
